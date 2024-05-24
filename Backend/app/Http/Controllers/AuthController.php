3bf4e48659a8668c9efb158e99f1ba222a9c440b
Adama <adamagassama40.ag@gmail.com>
2024-05-24 13:57:16 +0200

    feat: (AuthController) add login method

--- Backend/app/Http/Controllers/AuthController.php
+++ Backend/app/Http/Controllers/AuthController.php
@@ -4,18 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use PragmaRX\Google2FAQRCode\Google2FA;
 
 class AuthController extends Controller
 {
     //Add new user
-
     public function register(Request $request)
     {
         $request->validate([
             'name'  => 'required|string|max:255',
             'email' => 'required|string|max:255|email|unique:users',
-            'password' => 'required|string|min:8|confirmed'
+            'password' => 'required|string|min:8|confirmed',
         ]);
 
         $user = User::create([
@@ -25,5 +26,41 @@ class AuthController extends Controller
         ]);
         return response()->json(['message' => 'User registered successfully']);
     }
-    
+
+    // Add Login method
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = request(['email','password']);
+
+        if(!Auth::attempt($credentials)){
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $user = $request->user();
+
+        if($user){
+            $google2fa = new Google2FA();
+            $user->secret2fa = $google2fa->generateSecretKey();
+            $user->save();
+        }
+
+        return response()->json([
+            'message' => 'Login successful, enter your 2FA code',
+            'code_url' => $this->getQRCodeUrl($user->email, $user->secret2fa),
+        ]);
+    }
+
+    protected function getQRCodeUrl($email, $secret)
+    {
+        $google2fa = new Google2FA();
+        return $google2fa -> getQRCodeUrl(
+            'GassApp', $email, $secret
+        );
+    }
+
 }
