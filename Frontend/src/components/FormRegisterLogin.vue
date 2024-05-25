30c12a68bc6a358da009ed25e929f9f822ac184b
Adama <adamagassama40.ag@gmail.com>
2024-05-25 23:25:12 +0200

    feat: add CSS for Register and Login

--- Frontend/src/components/FormRegisterLogin.vue
+++ Frontend/src/components/FormRegisterLogin.vue
@@ -1,22 +1,22 @@
 <template>
 <div class="container" :class="{'register-mode': isSingUpMode }">
     <div class="forms-container">
-        <div class="register-login">
-            <form class="login-form" @submit.prevent="login">
-                <h2 class="tittle">Connexion</h2>
+        <div class="login-register">
+            <form class="login-form" @submit.prevent="handleLogin">
+                <h2 class="title">Connexion</h2>
                 <div class="input-field">
                     <i class="fas fa-user"></i>
-                    <input v-model="emailLogin" type="text" placeholder="Email" />
+                    <input v-model="emailLogin" type="text" placeholder="Email"  required/>
                 </div>
                 <div class="input-field">
                     <i class="fas fa-lock"></i>
-                    <input v-model="passwordLogin" type="password" placeholder="Password" />
+                    <input v-model="passwordLogin" type="password" placeholder="Password" required />
                 </div>
                 <input type="submit" value="Connexion" class="btn solid" />
             </form>
 
-            <form class="sign-up-form" @submit.prevent="register">
-                <h2 class="tittle">Inscription</h2>
+            <form class="register-form" @submit.prevent="handleRegister">
+                <h2 class="title">Inscription</h2>
                 <div class="input-field">
                     <i class="fas fa-user"></i>
                     <input v-model="name" type="text" placeholder="Nom Utilisateur" required/>
@@ -50,13 +50,18 @@
             </div>
         </div>
     </div>
+    <ModalForm v-if="showModal" :qrCodeUrl="qrCodeUrl" :tempToken="tempToken" @close="showModal = false" />
 </div>
 </template>
 
 <script>
 import axios from 'axios';
+import ModalForm from './ModalForm.vue';
 
 export default {
+    components: {
+       ModalForm
+    },
     data() {
         return {
             name: '',
@@ -65,10 +70,13 @@ export default {
             emailLogin: '',
             passwordLogin: '',
             isSingUpMode: false,
+            showModal: false,
+            qrCodeUrl: '',
+            tempToken: '',
         }
     },
     methods: {
-        async register() {
+        async handleRegister() {
             try {
                 await axios.post('http://127.0.0.1:8000/api/register', {
                     name: this.name,
@@ -76,20 +84,26 @@ export default {
                     password: this.password,
                 });
                 alert('Register successful. Please login !');
+                this.resetRegisterFields()
             } catch (error) {
                 console.error(error);
             }
         },
-        async login() {
+        async handleLogin() {
             try {
                    const response = await axios.post('http://127.0.0.1:8000/api/login', {
                     email: this.emailLogin,
                     password: this.passwordLogin,
                 });
                 if ( response.data.two_factor_required ) {
+                   //this.qrCodeUrl = response.data.qrCodeUrl;
+                   //this.tempToken = response.data.temp_token;
+                   //this.showModal = true;
                     this.$emit('show-2fa', response.data.temp_token);
+                    
                 } else {
                     alert(' Login successful ');
+                    this.resetLoginFields();
                 }
             } catch (error) {
                 console.error(error);
@@ -100,11 +114,20 @@ export default {
         },
         toggleSignInMode() {
             this.isSingUpMode = false
-        }
-    }
+        },
+        resetLoginFields(){
+            this.emailLogin = "";
+            this.passwordLogin = "";
+        },
+        resetRegisterFields(){
+            this.name ="";
+            this.email = "";
+            this.password = "";
+        },
+    },
 }
 </script>
 
 <style>
-/* @import '../assets/form.css'; */
+ @import '../assets/form.css';
 </style>
