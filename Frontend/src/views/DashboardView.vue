e59dfad23e32adcc08cf941c238d098871a8f82c
Adama <adamagassama40.ag@gmail.com>
2024-05-27 11:22:21 +0200

    feat: create DashboardView page

---
+++ Frontend/src/views/DashboardView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <DashboardHome />
+        <button @click ="logout">Logout</button>
+    </div>  
+</template>
+
+<script>
+import DashboardHome from '@/components/DashboardHome.vue';
+export default {
+    components: {
+        DashboardHome,
+    },
+    methods: {
+        logout() {
+            localStorage.setItem('isAuthenticated', 'false');
+            this.$router.push({ name: 'Auth' });
+        }
+    }
+}
+</script>
