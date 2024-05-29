c143a9abac949485cc529427fcab830ccacc7e26
Adama <adamagassama40.ag@gmail.com>
2024-05-29 17:51:38 +0200

    Refactor: (FormRegisterLogin) clean up modularize methods

--- Frontend/src/components/FormRegisterLogin.vue
+++ Frontend/src/components/FormRegisterLogin.vue
@@ -1,128 +1,122 @@
 <template>
-<div class="container" :class="{'register-mode': isSingUpMode }">
-    <div class="forms-container">
-        <div class="login-register">
-            <form class="login-form" @submit.prevent="handleLogin">
-                <h2 class="title">Connexion</h2>
-                <div class="input-field">
-                    <i class="fa fa-user"></i>
-                    <input v-model="emailLogin" type="email" placeholder="Email"  required/>
-                </div>
-                <div class="input-field">
-                    <i class="fas fa-lock"></i>
-                    <input v-model="passwordLogin" type="password" placeholder="Password" required />
-                </div>
-                <input type="submit" value="Connexion" class="btn solid" />
-            </form>
+    <div class="container" :class="{'register-mode': isSignUpMode }">
+        <div class="forms-container">
+            <div class="login-register">
+                <form class="login-form" @submit.prevent="handleLogin">
+                    <h2 class="title">Connexion</h2>
+                    <div class="input-field">
+                        <i class="fa fa-user"></i>
+                        <input v-model="emailLogin" type="email" placeholder="Email" required />
+                    </div>
+                    <div class="input-field">
+                        <i class="fas fa-lock"></i>
+                        <input v-model="passwordLogin" type="password" placeholder="Password" required />
+                    </div>
+                    <input type="submit" value="Connexion" class="btn solid" />
+                </form>
 
-            <form class="register-form" @submit.prevent="handleRegister">
-                <h2 class="title">Inscription</h2>
-                <div class="input-field">
-                    <i class="fas fa-user"></i>
-                    <input v-model="name" type="text" placeholder="Nom Utilisateur" required/>
-                </div>
-                <div class="input-field">
-                    <i class="fas fa-envelope"></i>
-                    <input v-model="email" type="email" placeholder="Email" required />
-                </div>
-                <div class="input-field">
-                    <i class="fas fa-lock"></i>
-                    <input v-model="password" type="password" placeholder="Password" required />
-                </div>
-                <input type="submit" value="Inscription" class="btn" />
-            </form>
-        </div>
-    </div>
-
-    <div class="panels-container">
-        <div class="panel left-panel">
-            <div class="content">
-                <h3>Nouvel Utilisateur ?</h3>
-                <p>Veuillez cliquer ci-dessous pour vous inscrire !</p>
-                <button class="btn transparent" @click="toggleSignUpMode">S'enregistrer</button>
+                <form class="register-form" @submit.prevent="handleRegister">
+                    <h2 class="title">Inscription</h2>
+                    <div class="input-field">
+                        <i class="fas fa-user"></i>
+                        <input v-model="name" type="text" placeholder="Nom Utilisateur" required />
+                    </div>
+                    <div class="input-field">
+                        <i class="fas fa-envelope"></i>
+                        <input v-model="email" type="email" placeholder="Email" required />
+                    </div>
+                    <div class="input-field">
+                        <i class="fas fa-lock"></i>
+                        <input v-model="password" type="password" placeholder="Password" required />
+                    </div>
+                    <input type="submit" value="Inscription" class="btn" />
+                </form>
             </div>
         </div>
-        <div class="panel right-panel">
-            <div class="content">
-                <h3>Déjà membre ?</h3>
-                <p>Veuillez cliquer ci-dessous pour vous connecter !</p>
-                <button class="btn transparent" @click="toggleSignInMode">Connexion</button>
+
+        <div class="panels-container">
+            <div class="panel left-panel">
+                <div class="content">
+                    <h3>Nouvel Utilisateur ?</h3>
+                    <p>Veuillez cliquer ci-dessous pour vous inscrire !</p>
+                    <button class="btn transparent" @click="toggleSignUpMode">S'enregistrer</button>
+                </div>
+            </div>
+            <div class="panel right-panel">
+                <div class="content">
+                    <h3>Déjà membre ?</h3>
+                    <p>Veuillez cliquer ci-dessous pour vous connecter !</p>
+                    <button class="btn transparent" @click="toggleSignInMode">Connexion</button>
+                </div>
             </div>
         </div>
-    </div>
 
-    <div>
-        <span></span>
+        <div>
+            <span></span>
+        </div>
     </div>
-</div>
 </template>
 
-<script>
-import axios from 'axios';
-import { toast } from 'vue3-toastify';
-import 'vue3-toastify/dist/index.css';
+<script setup>
+import { ref } from 'vue';
+import { registerUserApi, loginUserApi } from '@/services/apiService';
 
-export default {
-    data() {
-        return {
-            name: '',
-            email: '',
-            password: '',
-            emailLogin: '',
-            passwordLogin: '',
-            isSingUpMode: false
+const name = ref('');
+const email = ref('');
+const password = ref('');
+const emailLogin = ref('');
+const passwordLogin = ref('');
+const isSignUpMode = ref(false);
+const emit = defineEmits(['show-2fa']);
+
+const handleRegister = async () => {
+    try {
+        await registerUserApi({
+            name: name.value,
+            email: email.value,
+            password: password.value,
+        });
+        resetRegisterFields()
+    } catch (error) {
+        resetRegisterFields()
+    }
+};
+
+const handleLogin = async () => {
+    try {
+        const data = await loginUserApi({
+            email: emailLogin.value,
+            password: passwordLogin.value,
+        });
+        if (data.two_factor_required) {
+            emit('show-2fa', data.temp_token);
+        } else {
+            resetLoginFields();
         }
-    },
-    methods: {
-        async handleRegister() {
-            try {
-                await axios.post('http://127.0.0.1:8000/api/register', {
-                    name: this.name,
-                    email: this.email,
-                    password: this.password,
-                });
-                toast.success('Inscription avec succès. Connectez vous svp');
-                this.resetRegisterFields()
-            } catch (error) {
-                toast.error('Echec lors Inscription. Veuillez Reessayer SVP !', {autoClose: 2000})
-                this.resetRegisterFields()
-            }
-        },
-        async handleLogin() {
-            try {
-                   const response = await axios.post('http://127.0.0.1:8000/api/login', {
-                    email: this.emailLogin,
-                    password: this.passwordLogin,
-                });
-                if ( response.data.two_factor_required ) {
-                   this.$emit('show-2fa', response.data.temp_token);
-                } else {
-                    this.resetLoginFields();
-                }
-            } catch (error) {
-                toast.error('Identifiant ou mot de passe invalide. Veuillez Reessayer SVP!', {autoClose: 2000})
-                this.resetLoginFields()
-            }
-        },
-        toggleSignUpMode() {
-            this.isSingUpMode = true
-        },
-        toggleSignInMode() {
-            this.isSingUpMode = false
-        },
-        resetLoginFields(){
-            this.emailLogin = "";
-            this.passwordLogin = "";
-        },
-        resetRegisterFields(){
-            this.name ="";
-            this.email = "";
-            this.password = "";
-        },
-    },
-}
+        resetLoginFields();
+    } catch (error) {
+        resetLoginFields();
+    }
+};
+
+const toggleSignUpMode = () => {
+    isSignUpMode.value = true;
+};
+const toggleSignInMode = () => {
+    isSignUpMode.value = false;
+};
+const resetLoginFields = () => {
+    emailLogin.value = "";
+    passwordLogin.value = "";
+};
+const resetRegisterFields = () => {
+    name.value = "";
+    email.value = "";
+    password.value = "";
+};
+
 </script>
 
 <style>
- @import '../assets/form.css';
+@import '../assets/form.css';
 </style>
