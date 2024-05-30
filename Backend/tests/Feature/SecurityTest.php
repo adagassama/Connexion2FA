38d1e9860bd88e7e995df8cb6c1b6b61e6f5a36d
Adama <adamagassama40.ag@gmail.com>
2024-05-30 03:24:23 +0200

    feat: (Security) Update SecurityTest

--- Backend/tests/Feature/SecurityTest.php
+++ Backend/tests/Feature/SecurityTest.php
@@ -11,19 +11,20 @@ use Tests\TestCase;
 
 class SecurityTest extends TestCase
 {
+    //use RefreshDatabase;
     public function testSqlInjectionRegister()
     {
         // Attempt to register with SQL injection in the email
 
-        $response = $this->postJson('/register',[
-            'name' => 'Test User',
+        $response = $this->postJson('/api/register',[
+            'name' => 'Test User Register',
             'email' => "' OR '1'='1",
             'password' => Hash::make('password123'),
         ]);
 
         // Verification that inscription attempt fails
 
-        $response->assertStatus(404);
+        $response->assertStatus(422);
     }
 
     public function testSqlInjectionLogin()
@@ -31,18 +32,18 @@ class SecurityTest extends TestCase
         $randomEmail = 'user'.mt_rand(1,999).'@example.com';
         // Create Test User
         $user = User::factory()->create([
-            'name' => 'User Test',
+            'name' => 'User Test Login',
             'email' => $randomEmail,
             'password' => Hash::make('password123'),
         ]);
 
         // Attempt to connect with an SQL injection
-        $response = $this->postJson('/login',[
+        $response = $this->postJson('/api/login',[
             'email' => $randomEmail,
             'password' => "' OR '1'='1",
         ]);
 
         // Verification that the connection attempt fails
-        $response->assertStatus(404);
+        $response->assertStatus(401);
     }
 }
